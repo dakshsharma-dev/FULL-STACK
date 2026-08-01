2ff2fd6a6b1044a56d59513bece4705b1f80a8ee
Daksh Sharma <dakshsharma1016@gmail.com>
2026-08-01 15:18:04 +0530

    Added sidebar to youtube page

---
+++ JAVASCRIPT/01_Basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";  // treat all JS code as newer version of js
+
+// alert(3 + 3);  // we are using nodejs, not browser
+
+// EXAMPLES
+let name = "Daksh";
+let age = 20;
+let isLogggedIn = true;
+
+
+// PRIMITIVE DATATYPES     -> immutable
+    //1. Numeric
+        // number    // range: -(2^53 - 1) to (2^53 - 1)
+        // bigint
+    //2. Non-numeric
+        // string    -> '' or ""
+        // boolean
+        // null      -> standalone value
+        // undefined -> e.g. let state;
+        // symbol    -> uniqueness        -> mostly used in react
+
+
+// NON-PRIMITIVE DATATYPES
+    // object
+    // arrays
+    // function
+    // data object
+    // regular expression
+
+console.log(typeof(null));          // object
+console.log(typeof(undefined));     // undefined
+console.log(typeof(20));            // number
+console.log(typeof("Daksh"));
+console.log(typeof(symbol));        // undefined
+console.log(typeof(true));
+console.log(typeof(9323n));         // bigint
+
