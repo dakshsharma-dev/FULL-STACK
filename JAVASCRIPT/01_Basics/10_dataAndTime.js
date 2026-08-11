cee0cb846c236c8425ff30154267a451cd5437b8
Daksh Sharma <dakshsharma1016@gmail.com>
2026-08-11 23:49:20 +0530

    Learning Dates and Time in JS

---
+++ JAVASCRIPT/01_Basics/10_dataAndTime.js
@@ -0,0 +1,9 @@
+let myDate = new Date();  // Date instance/object
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
