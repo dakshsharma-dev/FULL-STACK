2343649dac4d2718dab930a38359b0f55ed56dec
Daksh Sharma <dakshsharma1016@gmail.com>
2026-08-23 15:13:46 +0530

    For While and Do While

---
+++ JAVASCRIPT/05_Iterations/forWhileDoWhile.js
@@ -0,0 +1,27 @@
+// for
+
+// for(let i = 1; i <= 10; i++){
+//     for(let j = 1; j <= 10; j++){
+//         console.log(`${i}*${j} = ${i*j}`);
+//     }
+// }
+
+
+let myArr = ["ironman", "captain", "spiderman", "flash"];
+
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+}
+
+// IMPORTANT NOTE: In JavaScript, accessing an array index that doesn't exist does not throw an error. It returns undefined.
+
+// while
+// while (condition) {
+    
+// }
+
+// do while
+// do {
+    
+// } while (condition);
